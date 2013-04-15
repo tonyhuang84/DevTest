dc9956a7abf454d2f177528ab09efb4e0baeb551
tony huang <tonyhuang84@gmail.com>
2013-04-15 09:57:35 +0800

    add show_ques_list() function

--- assets/www/testFunc.js
+++ assets/www/testFunc.js
@@ -6,10 +6,27 @@ function show_week_list()
 	{
 		html.push('<tr><td>');
 		html.push('<a href="#" class="btn large" onclick="alert(\'', i, '\');">', i,'</a><br>');
-//				html.push(i);
-//				alert(i);
+//		html.push(i);
 		html.push('</td></td>');
 	}
 	html.push('</table>');
-	document.getElementById("week_list").innerHTML = html.join('');
+	document.getElementById("item_list").innerHTML = html.join('');
+}
+
+function show_ques_list(qAryName)
+{
+//	alert(myQues[qAryName].length);
+
+	var qAry = myQues[qAryName];
+    var html = [''];
+	html.push('<br><br><table border="1">');
+	for (var i=0; i < qAry.length; i++)
+	{
+		html.push('<tr><td>');
+		alert(qAry[i][0]);
+		html.push('<a href="#" class="btn large" onclick="alert(\'', qAry[i][4], '\');">', qAry[i][0],'</a><br>');
+		html.push('</td></td>');
+	}
+	html.push('</table>');
+	document.getElementById("item_list").innerHTML = html.join('');
 }
